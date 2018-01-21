c11665384da7b59030cae5d1ca185e384789a90e
rebornlab <kevinng8383@gmail.com>
2018-01-21 12:47:56 +0700

    first

---
+++ a.js
@@ -0,0 +1,23 @@
+
+// 
+// Xem them tai lieu ve Javascript tai day
+// https://developer.mozilla.org/bm/docs/Web/JavaScript
+var x = 2
+console.log('Hello World ' + x)
+
+// cac kieu du lieu JS
+// 1. Javascript la ngon ngu kieu dong (dynamic types) -> 1 bien co the thay doi kieu khong mai mai chet 1 kieu INT thi sau nay khong the la STRING, Javascript co the thay doi a = 1; sau do co the la a = "String"
+// Trong class khong co  thuoc tinh  va Phuong Thuc
+let a = 1;
+console.log(a);
+a = "Hello"
+console.log(a);
+// deu chay tot  
+
+// 2. Kieu du lieu
+// Nhom kieu du lieu Nguyen Thuy : String, Number, Boolean, undefined, null
+// Nhom Object : objects, function, arrays
+// Su khac nhau giua kieu NGUYEN THUY va OBJ
+// NGUYEN THUY la tham tri
+// OBJ la tham chieu
+
